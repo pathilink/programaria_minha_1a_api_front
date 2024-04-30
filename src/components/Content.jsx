f39d6f3716d70353638e1dff4094c5a368a76c08
Patrícia <pathilink@gmail.com>
2024-04-30 01:39:43 -0300

    update render url

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -14,7 +14,7 @@ export function Content() {
   const [citacao, setCitacao] = useState('')
   const [imagem, setImagem] = useState('')
   const [success, setSuccess] = useState(false)
-  const baseURL = 'https://back-end-6c7c.onrender.com/mulheres'
+  const baseURL = 'https://programaria-minha-1a-api.onrender.com/mulheres'
 
   useEffect(() => {
     async function getData() {
